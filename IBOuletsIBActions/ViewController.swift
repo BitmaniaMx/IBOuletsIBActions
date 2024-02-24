30bd39e02b33a20b3e85f703b79348126c1351e1
rafaelgonzalez <rafael.glez@gmail.com>
2024-02-24 16:14:59 -0600

    Initial Version

--- IBOuletsIBActions/ViewController.swift
+++ IBOuletsIBActions/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pokemonImage: UIImageView!
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        pokemonImage.image = UIImage(named: "0")
+        
     }
-
-
+    
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        let pokemon = Int.random(in: 0..<24)
+        pokemonImage.image = UIImage(named: "\(pokemon)")
+        print("Button tapped!, selected image: \(pokemon)")
+    }
+    
+    
+    
 }
 
